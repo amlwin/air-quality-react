eab8bc088e841abd0dbedd3a96c4cb605b57f93d
aungmyolwin <bbannercoe@gmail.com>
2019-05-18 20:46:36 +0630

    add docs

---
+++ docs/precache-manifest.f4a5a04a83ee7c5c56519ca6917c9466.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911e9ec2134a2e582eb3bdefb9c21338",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2f5f8fc5624399db025",
+    "url": "/static/css/2.1ad4406c.chunk.css"
+  },
+  {
+    "revision": "e4699f5a61594c306532",
+    "url": "/static/css/main.86963caf.chunk.css"
+  },
+  {
+    "revision": "e2f5f8fc5624399db025",
+    "url": "/static/js/2.a0c60e56.chunk.js"
+  },
+  {
+    "revision": "e4699f5a61594c306532",
+    "url": "/static/js/main.630f8c5d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eeb9215184a60b20ad8eafbf730fc1a1",
+    "url": "/static/media/ic_humid.eeb92151.svg"
+  },
+  {
+    "revision": "9a1b2f6b27d1129f9172cf4f7d841d76",
+    "url": "/static/media/ic_location.9a1b2f6b.svg"
+  },
+  {
+    "revision": "e6b5c9a3d3615e9164f60d79cd02d18b",
+    "url": "/static/media/ic_mask_man.e6b5c9a3.svg"
+  },
+  {
+    "revision": "9f522932a1110feb31ac5c71da39558b",
+    "url": "/static/media/ic_pressure.9f522932.svg"
+  },
+  {
+    "revision": "0968992377b9defcd96c92ec262a958e",
+    "url": "/static/media/ic_temperature.09689923.svg"
+  },
+  {
+    "revision": "c933003831048c12e939b3c15103be37",
+    "url": "/static/media/ic_wind.c9330038.svg"
+  }
+]);
